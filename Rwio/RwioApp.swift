22aeeca4d0eea1e1a6660aae2b7571a3106f5555
aisel-mohbaliyeva <ayselmohbaliyeva22@gmail.com>
2026-09-02 17:09:28 +0400

    Initial Commit

---
+++ Rwio/RwioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RwioApp.swift
+//  Rwio
+//
+//  Created by Aysel Mohbaliyeva on 02.09.26.
+//
+
+import SwiftUI
+
+@main
+struct RwioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
